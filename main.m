e3808dac53f7c8db1c6d94edcde653ce1c275ac7
James Oldfield <hi@james-oldfield.co>
2017-11-16 19:02:29 +0000

    update data structures to store arbitrary architecture

--- main.m
+++ main.m
@@ -2,11 +2,39 @@ clear all;
 
 % define our inputs and ground truth values:
 X = [1 1
+     3 4
      1 0];
 y = [0 1];
 
 % define the initial weights provided by coursework,
 weights = [-0.2 0.15 -0.15 0.1 0.3 0.1 0.3 0.1 -0.3];
+
+% --------------------
+% SPECIFY ARCHITECTURE
+% --------------------
+
+% i.e. [2 3 1] specifies that 2 units are in the input layer,
+% 3 units in hidden layer
+% 1 unit in the output
+architecture = [2 3 1];
+
+% specify weights for linear terms
+linear_terms = rand(size(X, 2));
+
+% specify which activ fn we wish to use at each layer,
+% storing function handles.
+a_functions  = {@sigmoid, @sigmoid, @sigmoid};
+
+% create map of matrices to contain weights for every layer,
+% and corresponding derivatives
+weights   = containers.Map;
+d_weights = containers.Map;
+
+% populate weight mats with zeros to map from layer i to i+1
+for i = 1:length(architecture)-1
+    weights(int2str(i)) = rand(architecture(i), architecture(i+1));
+    d_weights(int2str(i)) = zeros(architecture(i), architecture(i+1));
+end
 
 % store previous gradients for momentum
 d_weights_old = zeros(1, 9);
@@ -17,6 +45,10 @@ beta = 0.2;
 n_epochs = 100;
 
 errors = zeros(1, n_epochs);
+
+% -----
+% TRAIN
+% -----
 
 for i_epoch = 1:n_epochs
     i_error = 0;
@@ -34,7 +66,7 @@ for i_epoch = 1:n_epochs
         % FORWARD PROP
         % ------------
         % calculate the activations at all nodes
-        activations = forward(x_example, weights);
+        activations = forward(x_example, weights, a_functions, linear_terms);
         
         % update the error for this batch (single example)
         i_error = i_error + abs(activations('out') - y_example);
